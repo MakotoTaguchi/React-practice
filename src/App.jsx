e5e204bbca40d58afb4905b119c8fcdbf7ff06d2
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2021-12-12 15:23:10 +0900

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
+import { ColoredMessage } from "./components/ColoredMessage";
+
 export const App = () => {
   const onClickButton = () => {
     alert();
   };
 
-  const contentStyle = {
-    color: "blue",
-    fontSize: "20px"
-  };
+  // const contentStyle = {
+  //   color: "blue",
+  //   fontSize: "20px"
+  // };
+
+  // const contentPinkStyle = {
+  //   color: "pink",
+  //   fontSize: "20px"
+  // };
 
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <p style={contentStyle}>お元気ですか？</p>
+      {/* <ColoredMessage color="blue" message="お元気ですか？" /> */}
+      <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
+      {/* <ColoredMessage color="pink" message="元気です！" /> */}
+      <ColoredMessage color="pink">元気です！</ColoredMessage>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );

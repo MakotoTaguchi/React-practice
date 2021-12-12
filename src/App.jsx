3e59084154a9382f6ffc85002b42aad3fbfea7c8
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2021-12-12 16:27:07 +0900

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,22 @@
+import { useState } from "react";
 import { ColoredMessage } from "./components/ColoredMessage";
 
 export const App = () => {
+  console.log("レンダリング");
+
+  const [num, setNum] = useState(0);
+
   const onClickButton = () => {
-    alert();
+    setNum(num + 1);
   };
-
-  // const contentStyle = {
-  //   color: "blue",
-  //   fontSize: "20px"
-  // };
-
-  // const contentPinkStyle = {
-  //   color: "pink",
-  //   fontSize: "20px"
-  // };
 
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
-      {/* <ColoredMessage color="blue" message="お元気ですか？" /> */}
       <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
-      {/* <ColoredMessage color="pink" message="元気です！" /> */}
       <ColoredMessage color="pink">元気です！</ColoredMessage>
       <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
     </>
   );
 };
